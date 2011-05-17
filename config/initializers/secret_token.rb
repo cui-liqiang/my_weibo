3c27377fce97ebe83641c635e79337f346fce047
Cui Liqiang <lqcui@thoughtworks.com>
2011-05-17 13:00:55 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWeibo::Application.config.secret_token = '1958a88b74e31bd0a9cffa1aa52112dbf02614552e2aaaa8894f795068f8a65b00f3b4c60c2d93fe3076ba2fd785117609ef8c56766b6d0aba0b023c8cdce4ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MyWeibo::Application.config.session_store :cookie_store, :key => '_my_weibo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MyWeibo::Application.config.session_store :active_record_store
